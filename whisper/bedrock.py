fc79c81f7f9c00450a8603f04e2131c2526a1bb9
fsantos98 <franciscoslb.98@hotmail.com>
2024-11-23 14:39:45 +0000

    cenas

--- whisper/bedrock.py
+++ whisper/bedrock.py
@@ -1,11 +1,17 @@
 import boto3
 import json
+from datetime import datetime
+
+current_day = datetime.now().day
+current_month = datetime.now().month
+current_year = datetime.now().year
+curr_date = str(current_day) + '/' + str(current_month) + '/' + str(current_year)
 
 def get_bedrock_completion(prompt):
     brt = boto3.client(service_name='bedrock-runtime')
 
     body = json.dumps({
-        "inputText": prompt,
+        "inputText": f"Extract the INITIAL_TIME and FINAL_TIME from the following text: {prompt}. The time should be in TIMESTAMP format in seconds. Today is {curr_date}",
         "textGenerationConfig": {
             "maxTokenCount": 3072,
             "stopSequences": [],
