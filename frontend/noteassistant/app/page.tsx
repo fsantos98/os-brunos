21327ccca47d7a718e248c1d4eec889a75e66f28
juniorvista1 <jorgejmartins42@gmail.com>
2024-11-23 00:40:51 +0000

    frontend

---
+++ frontend/noteassistant/app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Note Assistant</h1>
+      <p>Your personal note-taking assistant.</p>
+    </div>
+  );
+};
+
+export default HomePage;
